854e0a710da84bc4fefec8986feaf342468ea13f
Brendan Spinks <brendans@pageuppeople.com>
2015-04-08 14:28:56 +1000

    add output vars for instances

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "address" {
-  #value = "${aws_elb.elastic.dns_name}"
+output "instances private dns" {
+  value = "`${join(",", aws_instance.elastic.*.private_dns)}`"
 }
 
-output "private ip" {
-#  value = "${aws_instance.elastic.private_ip}"
+output "instances private ip" {
+  value = "`${join(",", aws_instance.elastic.*.private_ip)}`"
 }
 
-output "public ip" {
+#output "public ip" {
 #  value = "${aws_instance.elastic.public_ip}"
-}
+#}
